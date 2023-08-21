fd2576cab1dd4f63a19d6249fb50dae19dd0c550
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-21 15:24:19 -0300

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Shebang"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[0]
+    user_url = 'https://jsonplaceholder.typicode.com/todos/{}'.format(user_id)
+    user = requests.get(user_url).json()
+
+    todos_url = ('https://jsonplaceholder.typicode.com/todos/'
+                 .format(user_id))
+    todos = requests.get(todos_url).json()
+
+    completed_tasks = [task.get('title') for task in todos
+                       if task.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          len(completed_tasks), len(todos)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
